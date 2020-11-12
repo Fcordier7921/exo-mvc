3b36f9f7c4451dd94df4d1e2e25388d44cc6e6c6
Fcordier7921 <f.cordier@codeur.online>
2020-11-12 10:21:35 +0100

    instanciation controleur

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+// génére un econtance chemin vers index <div class="php"
+define('ROOT', str_replace('index.php', '', $_SERVER['SCRIPT8FILENAME'])).
+die(ROOT);
 // on  sépare les paramétres
 $params=explode('/',$_GET['p']);
-var_dump(($params));
+
+//est-ce qu'un paramétre existe
+if($params[0] != ""){
+    $controller =ucfirst($params[0]);
+
+    $action = isset($params[1]) ? $params[1] : 'index';
+
+    require_once(ROOT.'controlers/'.$controller.'.php');
+
+    $controller = new $controller();
+
+    $controller->$action();
+
+}else{
+
+}
